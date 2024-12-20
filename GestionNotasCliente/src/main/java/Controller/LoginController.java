93d707a675dc9e53289abbbf90187acc06de1173
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2024-12-20 00:55:30 -0500

    Actualizando el cliente

---
+++ GestionNotasCliente/src/main/java/Controller/LoginController.java
@@ -0,0 +1,50 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Dao.UsuarioDao;
+
+/**
+ * Servlet implementation class LoginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String usuario = request.getParameter("usuario");
+	    String contrasena = request.getParameter("contrasena");
+	    
+	    UsuarioDao loginDao = new UsuarioDao();
+	    boolean credencialesCorrectas = loginDao.IniciarSession(usuario, contrasena);
+	    
+	    if (credencialesCorrectas) {
+	        response.sendRedirect("index.jsp");
+	    } else {
+	        response.sendRedirect("login.jsp?error=1");
+	    }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
